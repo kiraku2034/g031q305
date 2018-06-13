763e42b767eb6b3b50280916f506f262a37107dc
Kiraku Saga <g031q305@s.iwate-pu.ac.jp>
2018-06-13 12:15:14 +0900

    訂正

---
+++ fizzbuzz.php
@@ -0,0 +1,13 @@
+<?php
+
+for ($a = 1; $a <= 100; $a++) {
+       if ($a % 3 == 0) 
+            echo 'fizz';
+       if ($a % 5 == 0)
+            echo 'buzz';
+       if (!($a % 3 == 0) && !($a % 5 == 0)) {
+            echo $a;
+       }
+       echo "<br/>";
+}
+?>
